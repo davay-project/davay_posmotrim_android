7699a526276a0363815d6dacdc0d9bfe8fe213cb
DimaB <cedricga@school21>
2024-03-07 19:26:21 +0300

    detect trying

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("io.gitlab.arturbosch.detekt")
 }
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.2.2")
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
 
 android {
     namespace = "com.davayposmotrim.android"
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,19 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.android.library") version "8.2.2" apply false
+    id("io.gitlab.arturbosch.detekt") version("1.23.3")
 }
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.5")
+    }
+
+}
+
