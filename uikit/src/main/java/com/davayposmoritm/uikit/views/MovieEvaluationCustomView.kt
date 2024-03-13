1b401fd4a9fd7a475192ffc9da2c7014eaaf1800
Eva M <krageleva67@gmail.com>
2024-03-13 14:20:52 +0300

    added a color change

--- uikit/src/main/java/com/davayposmoritm/uikit/views/MovieEvaluationCustomView.kt
+++ uikit/src/main/java/com/davayposmoritm/uikit/views/MovieEvaluationCustomView.kt
@@ -7,6 +7,7 @@ import android.util.AttributeSet
 import android.widget.TextView
 import androidx.annotation.StyleableRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.uikit.R
 
 class MovieEvaluationCustomView @JvmOverloads constructor(
@@ -15,22 +16,46 @@ class MovieEvaluationCustomView @JvmOverloads constructor(
     defStyleAttr: Int = 0,
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    val rating1IMDb = findViewById<TextView>(R.id.ratingIMDbTextView)
-
+    private val ratingIMDbTextView: TextView
+    private val stringRatingIMDbTextView: TextView
 
     init {
         inflate(context, R.layout.movie_evaluation_custom_view, this)
 
+        ratingIMDbTextView = findViewById(R.id.ratingIMDbTextView)
+        stringRatingIMDbTextView = findViewById(R.id.stringRatingIMDbTextView)
+
         attrs?.applyStyleable(context, R.styleable.MovieEvaluationCustomView) {
-            rating1IMDb.text = getString(R.styleable.MovieEvaluationCustomView_rating)
-            rating1IMDb.setTextColor(getColor(R.styleable.MovieEvaluationCustomView_color, Color.BLACK))
+            val rating =
+                getString(R.styleable.MovieEvaluationCustomView_rating)?.toFloatOrNull() ?: 0f
+            val color = getRatingColor(rating)
+
+            ratingIMDbTextView.text = R.string.rating_IMDb.toString()
+            ratingIMDbTextView.text = rating.toString()
+            ratingIMDbTextView.setBackgroundColor(color)
+        }
+
+
+    }
+
+    private fun getRatingColor(rating: Float): Int {
+        return when (rating) {
+            in 7.0f..10.0f -> ContextCompat.getColor(context, R.color.accent_secondary_base)
+            in 5.0f..6.9f -> ContextCompat.getColor(context, R.color.additional_attention)
+            in 0.0f..4.9f -> ContextCompat.getColor(context, R.color.additional_error)
+            else -> ContextCompat.getColor(
+                context,
+                androidx.appcompat.R.color.primary_material_light
+            )
         }
     }
 }
 
-inline fun AttributeSet.applyStyleable(context: Context,
-                                       @StyleableRes styleableResId: IntArray,
-                                       action: TypedArray.() -> Unit) {
+inline fun AttributeSet.applyStyleable(
+    context: Context,
+    @StyleableRes styleableResId: IntArray,
+    action: TypedArray.() -> Unit
+) {
 
     val typedArray = context.obtainStyledAttributes(this, styleableResId)
     typedArray.action()
