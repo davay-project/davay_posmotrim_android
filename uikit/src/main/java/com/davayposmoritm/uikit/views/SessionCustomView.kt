7ecfe76668ff5d802eb61e5f2d3a14e5e1e6936a
Eva M <krageleva67@gmail.com>
2024-03-18 13:58:23 +0300

    add image

--- uikit/src/main/java/com/davayposmoritm/uikit/views/SessionCustomView.kt
+++ uikit/src/main/java/com/davayposmoritm/uikit/views/SessionCustomView.kt
@@ -2,11 +2,15 @@ package com.davayposmoritm.uikit.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
 import com.davayposmoritm.uikit.views.ext.applyStyleable
 import com.example.uikit.R
 
-class SessionCustomView@JvmOverloads constructor(
+
+class SessionCustomView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
@@ -15,7 +19,23 @@ class SessionCustomView@JvmOverloads constructor(
         inflate(context, R.layout.session_custom_view, this)
 
         attrs?.applyStyleable(context, R.styleable.SessionCustomView) {
+            findViewById<TextView>(R.id.titleSessionTextView).text =
+                getString(R.styleable.SessionCustomView_titleSession)
 
+            findViewById<TextView>(R.id.subtitleSessionTextView).text =
+                getString(R.styleable.SessionCustomView_subtitleSession)
+
+            findViewById<TextView>(R.id.countMatchesSessionTextView).text =
+                getString(R.styleable.SessionCustomView_matchesCount)
+
+            val imageUrl = attrs?.getAttributeValue("http://schemas.android.com/apk/res-auto", "imageUrl")
+
+            // Проверяем, что imageUrl не равен null и загружаем изображение с помощью Glide
+            imageUrl?.let {
+                Glide.with(context)
+                    .load(imageUrl)
+                    .into(findViewById(R.id.imageSession))
+            }
         }
     }
 }
