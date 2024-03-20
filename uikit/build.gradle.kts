29640233a695be6943d6d11043aadd93d0f104d9
Eva M <krageleva67@gmail.com>
2024-03-20 09:37:09 +0300

    fix gradle

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -35,10 +35,8 @@ android {
 
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.3")
-    //glide
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
